7a1b5605f04b72e5c02a411e243bc4e07fb003c6
Francisco Alvidrez <falvid16@gmail.com>
2022-07-12 11:59:00 -0600

    Initial project commit with postgresdb settings configured and env variables set up

---
+++ movies/settings/dev.py
@@ -0,0 +1,9 @@
+from .settings import *
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'django-insecure-_i#&$x1t2z5j^ygp50^tdl)51o9j&s8h(^+z&r#e6+k9ei(lmb'
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+ALLOWED_HOSTS = []
