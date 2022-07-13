4f3b1ab9e877492b1fd757f5a38104772a159d06
Francisco Alvidrez <falvid16@gmail.com>
2022-07-13 12:04:51 -0600

    Created the registration view

--- users/views.py
+++ users/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from .forms import RegistrationForm
 
 # Create your views here.
+def register(requests):
+    if requests.method == 'POST':
+        form = RegistrationForm(requests.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('/users/login')
+    else:
+        form = RegistrationForm()
+    
+    return render('users/register.html', context={'form':form})
