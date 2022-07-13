f71ec20fa68d4a5f3ea54a44d5cffd756a479fd0
Francisco Alvidrez <falvid16@gmail.com>
2022-07-13 11:36:45 -0600

    Added the __str__ methods to the models

--- movies_app/models.py
+++ movies_app/models.py
@@ -2,12 +2,21 @@ from django.db import models
 
 # Create your models here.
 class Genre(models.Model):
-    genre = models.CharField(max_length=10)
+    genre = models.CharField(max_length=10, unique=True)
+    
+    def __str__(self):
+        return self.genre
 
 class Movie(models.Model):
     title = models.CharField(max_length=20, unique=True)
     genre = models.ManyToManyField(Genre)
 
+    def __str__(self):
+        return self.title
+
 class Character(models.Model):
     name = models.CharField(max_length=20)
-    movies = models.ForeignKey(Movie, null=True, blank=True, on_delete=models.CASCADE)
+    movie = models.ForeignKey(Movie, null=True, blank=True, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
