43270c421ebd6e4203a7a711b8bb115847dd7cc7
Francisco Alvidrez <falvid16@gmail.com>
2022-07-13 11:38:37 -0600

    Added the models to the forms

--- movies_app/forms.py
+++ movies_app/forms.py
@@ -1,14 +1,18 @@
 from django.forms import ModelForm
+from .models import Character, Movie, Genre
 
 class GenreForm(ModelForm):
     class Meta:
+        model = Genre
         fields = ['genre']
 
 class MovieForm(ModelForm):
     class Meta:
+        model = Movie
         fields = ['title', 'genre']
 
 class CharacterForm(ModelForm):
     class Meta:
+        model = Character
         fields = ['name', 'movie' ]
 
