a40695a55ab181f716326754bc0a8f069e962b23
Francisco Alvidrez <falvid16@gmail.com>
2022-07-13 00:37:14 -0600

    Created the list vies for characters, movies, and genres

--- movies_app/views.py
+++ movies_app/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from .forms import GenreForm, MovieForm, CharacterForm
+from .models import Character, Movie, Genre
 
 # Create your views here.
 def addGenre(requests):
@@ -34,3 +35,15 @@ def addCharacter(requests):
         form = CharacterForm()
 
     return render(requests, 'movies/add_genre.html', context={'form':form})
+
+def genreList(requests):
+    genres = Genre.objects.all 
+    return render(requests, 'movies/genres.html', context={'genres':genres})
+
+def movieList(requests):
+    movies = Movie.objects.all 
+    return render(requests, 'movies/movies.html', context={'movies':movies})
+
+def characterList(requests):
+    characters = Character.objects.all 
+    return render(requests, 'movies/characters.html', context={'characters':characters})
