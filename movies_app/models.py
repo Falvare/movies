7dd18a2f77b96e5e8fda85b0b6b1924130307163
Francisco Alvidrez <falvid16@gmail.com>
2022-07-12 23:23:35 -0600

    Created the models for the movie app

--- movies_app/models.py
+++ movies_app/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Genre(models.Model):
+    genre = models.CharField(max_length=10)
+
+class Movie(models.Model):
+    title = models.CharField(max_length=20, unique=True)
+    genre = models.ManyToManyField(Genre)
+
+class Character(models.Model):
+    name = models.CharField(max_length=20)
+    movies = models.ForeignKey(Movie, null=True, blank=True, on_delete=models.CASCADE)
