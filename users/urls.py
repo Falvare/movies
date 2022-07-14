aa3a083b77658fc8dacaf7dea5c1d8d3d3fee1f7
Francisco Alvidrez <falvid16@gmail.com>
2022-07-14 14:12:46 -0600

    Added the logout url to url patterns

--- users/urls.py
+++ users/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from .views import register, Login, profile
+from django.contrib.auth.views import LogoutView
 
 urlpatterns = [
     path('register/', register, name='register' ),
     path('login/', Login.as_view(), name='login' ),
     path('profile/', profile, name='profile' ),
+    path('logout/', LogoutView.as_view(), name='logout' ),
 ]
