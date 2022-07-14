d2e17e93b4526da67063ed295e0a5261d35e68dc
Francisco Alvidrez <falvid16@gmail.com>
2022-07-14 12:00:02 -0600

    Removed uneccesary slash from url

--- movies/urls.py
+++ movies/urls.py
@@ -18,6 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('/', include('movies_app.urls')),
+    path('', include('movies_app.urls')),
     path('users/', include('users.urls'))
 ]
