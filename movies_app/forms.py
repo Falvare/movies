ffd86844ce331ba5f70021474ca8f5963b11454e
Francisco Alvidrez <falvid16@gmail.com>
2022-07-14 14:03:06 -0600

    Changed the genre select on movie form to checkboxes

--- movies_app/forms.py
+++ movies_app/forms.py
@@ -1,4 +1,5 @@
 from django.forms import ModelForm
+from django.forms.widgets import CheckboxSelectMultiple
 from .models import Character, Movie, Genre
 
 class GenreForm(ModelForm):
@@ -10,6 +11,14 @@ class MovieForm(ModelForm):
     class Meta:
         model = Movie
         fields = ['title', 'genre']
+
+    def __init__(self, *args, **kwargs):
+    
+        super(MovieForm, self).__init__(*args, **kwargs)
+        
+        self.fields["genre"].widget = CheckboxSelectMultiple()
+        self.fields["genre"].queryset = Genre.objects.all()
+
 
 class CharacterForm(ModelForm):
     class Meta:
