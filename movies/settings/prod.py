7dc8db96f71a762bc6430decbd6bae6870854df4
Francisco Alvidrez <falvid16@gmail.com>
2022-07-15 00:54:49 -0600

    Added the domain name to allowed hosts

--- movies/settings/prod.py
+++ movies/settings/prod.py
@@ -7,4 +7,8 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['dj-characters.herokuapp.com', '127.0.0.1']
+
+CSRF_TRUSTED_ORIGINS = [
+    'https://dj-characters.herokuapp.com/'
+]
