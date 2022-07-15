f9837eaf35866051ffe21c2863efbc2b7f027d66
Francisco Alvidrez <falvid16@gmail.com>
2022-07-14 21:35:35 -0600

    Changed the add character url to include the movie details

--- movies_app/urls.py
+++ movies_app/urls.py
@@ -4,10 +4,10 @@ from .views import addGenre, addCharacter, addMovie, genreList, characterList, m
 urlpatterns = [
     path('add-genre/', addGenre, name='add genre'),
     path('add-movie/', addMovie, name='add movie'),
-    path('add-character/', addCharacter, name='add character'),
     path('genres/', genreList, name='genres'),
     path('', movieList, name='movies'),
     path('movies/', movieList, name='movies'),
-    path('movie-details/<int:pk>', movieDetails, name='movie details'),
+    path('movie-details/<int:pk>/', movieDetails, name='movie details'),
+    path('movie-details/<int:pk>/add-character', addCharacter, name='add character'),
     path('characters/', characterList, name='characters'),
 ]
